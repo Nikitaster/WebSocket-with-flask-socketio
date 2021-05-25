5e19dd3f2f147408a09ba21845c753832dc8dc9d
Nikita Gudkov <nikitaster2001@gmail.com>
2021-05-25 20:22:16 +0000

    Update gunicorn-run.sh

--- gunicorn-run.sh
+++ gunicorn-run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source venv/bin/activate
-gunicorn -k geventwebsocket.gunicorn.workers.GeventWebSocketWorker -w 10 app:app --bind 0.0.0.0:8000
+gunicorn -k geventwebsocket.gunicorn.workers.GeventWebSocketWorker -w 10 app:app --bind 0.0.0.0:8002
 
